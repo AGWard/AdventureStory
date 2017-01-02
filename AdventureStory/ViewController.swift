afbf757f3f0e62080a9a76d87c75d87c4aa0247f
AGWard <anthony.ward.data@gmail.com>
2017-01-02 14:40:53 -0400

    Initial Commit

---
+++ AdventureStory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdventureStory
+//
+//  Created by Anthony Ward on 1/2/17.
+//  Copyright © 2017 AppCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
